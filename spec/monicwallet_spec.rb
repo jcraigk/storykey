9beaf8fb7f25fc28366827cd892d7ffe93f53608
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2022-03-12 20:31:44 -0800

    Initial commit

---
+++ spec/monicwallet_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Monicwallet do
+  it "has a version number" do
+    expect(Monicwallet::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
